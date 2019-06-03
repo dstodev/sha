679186afbdd61bc61db77175c26667ba581c0330
Daniel Stotts <daniel.stotts@labs.cset.oit.edu>
2019-06-03 11:27:32 -0700

    Converted to command-line utility

--- project/source/main.c
+++ project/source/main.c
@@ -1,25 +1,43 @@
+#ifdef _WIN32
+#define _CRT_SECURE_NO_WARNINGS
+#define strdup _strdup
+static char * sep = "\\";
+#else
+static char * sep = "/";
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "sha.h"
 
 int main(int argc, char * argv[])
 {
+	char * exe = 0;
+	char * token = 0;
+	unsigned char * d = 0;
+
 	if (argc > 1) {
-		printf("Parameters:\n");
 		for (int i = 1; i < argc; ++i) {
-			printf("%d. %s\n", i, argv[i]);
+			d = digest(argv[i]);
+
+			for (int j = 0; j < 32; ++j) {
+				printf("%02x", d[j]);
+			}
+			printf("\n");
+			free(d);
 		}
-		printf("\n");
+	} else {
+		// Get executable basename
+		token = strtok(argv[0], sep);
+		exe = strdup(token);
+		while ((token = strtok(0, sep)) != 0) {
+			exe = strdup(token);
+		}
+		fprintf(stderr, "Usage: %s <input> [... input]\n", exe);
+		free(exe);
 	}
-
-	unsigned char * d = digest("test");
-
-	for (int i = 0; i < 32; ++i) {
-		printf("%02x ", d[i]);
-	}
-
-	free(d);
 
 	return 0;
 }
